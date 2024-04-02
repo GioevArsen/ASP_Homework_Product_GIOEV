86b9bd4186c850f3ae67a2f5ca66558fbf167b2e
GioevArsen <gioev.arsen15@list.ru>
2024-04-03 00:38:06 +0300

    Added comparison feature

---
+++ ASP_Homework_Product/Controllers/ComparisonController.cs
@@ -0,0 +1,31 @@
+﻿using ASP_Homework_Product.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_Homework_Product.Controllers
+{
+    public class ComparisonController : Controller
+    {
+        private readonly IProductsRepository productsRepository;
+
+        public ComparisonController(IProductsRepository productsRepository)
+        {
+            this.productsRepository = productsRepository;
+        }
+
+        public IActionResult Index(int productId, int compareId = -1)
+        {
+            var product = productsRepository.TryGetById(productId);
+            Product comparableProduct;
+            if(compareId > -1)
+            {
+                comparableProduct = productsRepository.TryGetById(compareId);
+            }
+            else
+            {
+                comparableProduct = productsRepository.TryGetById(productId);
+            }
+            var viewModel = new ComparisonItem(product, comparableProduct, productsRepository);
+            return View(viewModel);
+        }
+    }
+}
---
+++ ASP_Homework_Product/Models/ComparisonItem.cs
@@ -0,0 +1,17 @@
+﻿namespace ASP_Homework_Product.Models
+{
+    public class ComparisonItem
+    {
+        public Product Product { get; }
+
+        public Product ComparableProduct { get; }
+        public IProductsRepository ProductsRepository { get; }
+
+        public ComparisonItem(Product product, Product comparableProduct, IProductsRepository productsRepository)
+        {
+            Product = product;
+            ComparableProduct = comparableProduct;
+            ProductsRepository = productsRepository;
+        }
+    }
+}
