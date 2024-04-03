645d4aed0f85c0b73eba5cd83003990ba7ac42f7
GioevArsen <gioev.arsen15@list.ru>
2024-04-03 23:17:05 +0300

    Task have already been complited at 5_2 branch

--- ASP_Homework_Product/Controllers/AccountController.cs
+++ ASP_Homework_Product/Controllers/AccountController.cs
@@ -21,7 +21,7 @@ namespace ASP_Homework_Product.Controllers
 
         public string CreateNewAccount(string name, string lastName, string email, string password, bool agreement)
         {
-            return $"{name} {lastName} your account successfully created. Check the letter we send you to {email}.\n Your password: {password}.\n Aggrement: {agreement}";
+            return $"{name} {lastName} your account successfully created! Check the letter we send you to {email}.\n Your password: {password}.\n Aggrement: {agreement}";
         }
 
         public string LogIntoAccount(string login, string password, bool remember)
