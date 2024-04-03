51c67efc3892625929908ad52218f79654ff5646
GioevArsen <gioev.arsen15@list.ru>
2024-04-03 23:14:04 +0300

    Added login and signup forms

---
+++ ASP_Homework_Product/Controllers/AccountController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_Homework_Product.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult LogIn()
+        {
+            return View();
+        }
+
+        public IActionResult LogOut()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        public string CreateNewAccount(string name, string lastName, string email, string password, bool agreement)
+        {
+            return $"{name} {lastName} your account successfully created. Check the letter we send you to {email}.\n Your password: {password}.\n Aggrement: {agreement}";
+        }
+
+        public string LogIntoAccount(string login, string password, bool remember)
+        {
+            return $"{login} - {password} - {remember}";
+        }
+    }
+}
